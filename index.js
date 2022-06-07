2602448e59a7d1410da9f1700ea16293adb57296
Aden Addis <adenwonde56@gmail.com>
2022-06-07 13:24:15 -0400

    commit tree ad5fe7f1ccc5de666e3f7d2482eeda2da1a6925d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names, event) {
+    const thankuCards = []
+    for(let i= 0; i < names.length; i++) {
+   thankuCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankuCards
+}
+
+// function writecards()
+// console.log(writeCards(names, "aniversary"))
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--){
+        console.log(i)
+    }
+}
